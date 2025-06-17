3c440dc67d9907880fdc533b9aed9c1c95c7cf91
abutler911 <abutler911@gmail.com>
2025-06-17 12:29:02 -0600

    Feat: Added reporters and new topics

--- server/utils/topics.js
+++ server/utils/topics.js
@@ -1,5 +1,7 @@
 // server/utils/topics.js
+
 module.exports = [
+  // Core Topics
   "US politics",
   "global economy",
   "tech regulation",
@@ -12,4 +14,32 @@ module.exports = [
   "military industrial complex",
   "free speech",
   "media bias",
+
+  // Society & Culture
+  "civil unrest",
+  "misinformation & propaganda",
+  "culture wars",
+  "digital privacy",
+  "mental health crisis",
+  "income inequality",
+  "corporate overreach",
+
+  // Tech & Future
+  "automation & jobs",
+  "biohacking & longevity",
+  "space colonization",
+  "deep learning",
+  "quantum computing",
+
+  // Environment & Collapse
+  "water scarcity",
+  "environmental collapse",
+  "post-capitalist transition",
+
+  // Power & Institutions
+  "election integrity",
+  "shadow governance",
+  "prison industrial complex",
+  "whistleblower suppression",
+  "disaster capitalism",
 ];
